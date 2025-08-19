8d5f5ce3b7ae2107d0f52b7d4cf6165c3b58e51c
poojithasidda <poojithasidda36@gmail.com>
2025-08-19 14:02:39 +0530

    Add files via upload

---
+++ BubbleSort.java
@@ -0,0 +1,21 @@
+class BubbleSort {
+    public static void main(String[] args) {
+        int[] arr = {5, 2, 9, 1, 5, 6};
+        System.out.println("Sorted array:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+                }
+            }
+            public static void BubbleSort(int[] arr) {
+                int n = arr.length;
+                for (int i = 0; i < n - 1; i++) {
+                    for (int j = 0; j < n - 1 - i; j++) {
+                        if (arr[j] > arr[j + 1]) {
+                            int temp = arr[j];
+                            arr[j] = arr[j + 1];
+                            arr[j + 1] = temp;
+                        }
+                    }
+                }
+            }
+        }
